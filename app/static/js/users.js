466e83317617e884da787857d1b14dfc0400b0c4
Worlako Dzokoto <worlaco.dzo@gmail.com>
2023-01-27 16:04:35 +0000

    Update ingress metric endpoint

--- app/static/js/users.js
+++ app/static/js/users.js
@@ -16,7 +16,24 @@ document.addEventListener("DOMContentLoaded", function(event){
 
 
 const loadUsers = () => {
-    
+
+    fetch('/users', {
+        method: "GET",
+        headers: {"Content-Type": "application/json"}
+    }).then(res => {
+
+        if (res.status == 200){
+            // Convert data to json
+            return res.json();
+        }else{
+
+        }
+
+
+    }).then(res => {
+        
+    })
+
 }
 
 
